a9a2c573f556edf0815facc267a47492c8409b29
ImeliaDwinora <imeliadwinora@gmail.com>
2025-07-13 04:52:20 +0700

    nambah

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -30,7 +30,7 @@ export default function Contact() {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 text-center justify-center items-center mb-6">
         <div className="relative w-[300px] h-[300px] mx-auto md:w-[400px] md:h-[400px]">
           <Image
-            src="/contact.png"
+            src="/properties/contact.png"
             alt="Contact Illustration"
             fill
             className="object-contain rotate-[-7deg] hover:rotate-7 transition-transform duration-200 animate-float"
@@ -39,12 +39,12 @@ export default function Contact() {
 
         {/* Kontak Kanan */}
         <div className="relative w-full max-w-md mx-auto aspect-[4/3]">
-          <Image src="/box.png" alt="Box Background" fill className="object-contain" />
+          <Image src="/properties/box.png" alt="Box Background" fill className="object-contain" />
 
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full px-4 flex flex-col items-center justify-center gap-6 z-10 text-[#2E4057] font-bold">
             <div className="flex flex-col sm:flex-row items-center justify-center gap-2 sm:gap-4 text-center">
               <a href={whatsappLink} target="_blank" className="hover:rotate-7 transition-transform duration-200">
-                <Image src="/app.png" alt="Phone Icon" width={40} height={40} className="w-[24px] h-[24px] md:w-[40px] md:h-[40px]" />
+                <Image src="/icons/app.png" alt="Phone Icon" width={40} height={40} className="w-[24px] h-[24px] md:w-[40px] md:h-[40px]" />
               </a>
               <a href={whatsappLink} target="_blank">
                 <h3 className="text-sm md:text-2xl break-words">{content.phone}</h3>
@@ -53,7 +53,7 @@ export default function Contact() {
 
             <div className="flex flex-col sm:flex-row items-center justify-center gap-2 sm:gap-4 text-center">
               <a href={`mailto:${content.email}`} className="hover:rotate-7 transition-transform duration-200">
-                <Image src="/gmail.png" alt="Email Icon" width={40} height={40} className="w-[24px] h-[24px] md:w-[40px] md:h-[40px]" />
+                <Image src="/icons/gmail.png" alt="Email Icon" width={40} height={40} className="w-[24px] h-[24px] md:w-[40px] md:h-[40px]" />
               </a>
               <a href={`mailto:${content.email}`}>
                 <h3 className="text-sm md:text-2xl break-words">{content.email}</h3>
